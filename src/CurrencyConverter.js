fe75451e7113ead8edc2daa81a80ffd8f7a7193c
WarlockHolmes <66439068+WarlockHolmes@users.noreply.github.com>
2020-10-25 17:22:56 -0600

    Base widget react structure

---
+++ src/CurrencyConverter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { json, checkStatus } from './utils';
+
+
+class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      base: null,
+    }
+  }
+
+  componentDidMount () {
+      fetch(``)
+        .then(checkStatus)
+        .then(json)
+        .then((data) => {
+          if (data.Response === 'False') {
+            throw new Error(data.Error);
+          }
+
+          if (data.Response === 'True') {
+            console.log(data);
+            this.setState({ });
+          }
+        })
+        .catch((error) => {
+          this.setState({ error: error.message });
+          console.log(error);
+        })
+    }
+
+  render() {
+
+    return
+  }
+}
+
+export default CurrencyConverter;
