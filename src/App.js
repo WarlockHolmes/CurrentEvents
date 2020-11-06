3f11a35502913752c2f369ea932dc61863436d95
WarlockHolmes <66439068+WarlockHolmes@users.noreply.github.com>
2020-11-06 12:53:46 -0700

    Functioning Nav Buttons
    Desktop view nav buttons now show/hide Converter and Exchange Rate components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Template from './template';
-import Home from './Home';
 import CurrencyConverter from './CurrencyConverter';
 import ExchangeRates from './ExchangeRates';
 import About from './About';
@@ -13,21 +12,55 @@ const NotFound = () => {
   return <h2>404 Not Found</h2>;
 }
 
-const App = () => {
-  return (
-    <Router>
-      <Template>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/converter" component={CurrencyConverter} />
-          <Route path="/rates" component={ExchangeRates} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
-      </Template>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showConverter: false,
+      showRates: false,
+    }
+    this.toggleConverter = this.toggleConverter.bind(this);
+    this.toggleRates = this.toggleRates.bind(this);
+  }
+
+  toggleConverter() {
+    this.setState({ showConverter: !this.state.showConverter });
+  }
+
+  toggleRates() {
+    this.setState({ showRates: !this.state.showRates });
+  }
+
+  render() {
+    const { showRates, showConverter } = this.state;
+    const Home = () => {
+      return (
+        <React.Fragment>
+          <h2>Home</h2>
+          <div id="rates">
+            { showRates && <p>ExchangeRates</p> }
+          </div>
+          <div id="converter">
+            { showConverter && <CurrencyConverter /> }
+          </div>
+        </React.Fragment>
+      );
+    }
+    return (
+      <Router>
+        <Template toggleConverter={this.toggleConverter} toggleRates={this.toggleRates}>
+          <Switch>
+            <Route path="/" exact render={Home}/>
+            <Route path="/converter" component={CurrencyConverter} />
+            <Route path="/rates" component={ExchangeRates} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </Template>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/template.js
+++ src/template.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import "./template.css"
+import './template.css'
+
 
 const SocialMedia = () => {
   return (
     <React.Fragment>
-      <a href="facebook.com"><i class="fab fa-facebook-square"></i></a>
-      <a href="github.com"><i class="fab fa-github-square"></i></a>
-      <a href="linkedin.com"><i class="fab fa-linkedin"></i></a>
+      <a href="facebook.com"><i className="fab fa-facebook-square"></i></a>
+      <a href="github.com"><i className="fab fa-github-square"></i></a>
+      <a href="linkedin.com"><i className="fab fa-linkedin"></i></a>
     </React.Fragment>
   );
 }
@@ -23,27 +24,31 @@ const MoreInfo = () => {
   );
 }
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-lg border-bottom">
+
       <div className="d-none d-md-flex w-100" id="desktopNav">
         <h2><Link to="/">CurrentEvents</Link></h2>
         <div className="ml-auto">
           <button className="btn btn-outline-secondary">Exchange Rates</button>
-          <button className="btn btn-outline-secondary">Currency Converter</button>
+          <button className="btn btn-outline-secondary" onClick={props.toggleConverter}>Currency Converter</button>
         </div>
       </div>
+
       <div className="d-flex d-md-none" id="mobileNav">
         <h1>Mobile Nav</h1>
       </div>
+
     </nav>
   );
 }
 
 const Template = (props) => {
+
   return (
     <React.Fragment>
-      <Navbar />
+      <Navbar toggleConverter={props.toggleConverter} toggleRates={props.toggleRates}/>
       <div className="container-fluid px-0 w-100 d-none d-md-block" id="desktop">
         <div className="row h-100 w-100">
           <div className="col-12">
